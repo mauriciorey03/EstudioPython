fb7c26e4d19dba6a5afb91ad10e7c6505a2f0416
mauriciorey03 <edgarmeza909@gmail.com>
2023-08-16 11:48:23 -0500

    Estudio en casa

--- Teoría/4. proyecto estudio.py
+++ Teoría/4. proyecto estudio.py
@@ -1,41 +1,63 @@
-clientes='pablo,ricardo,'
+clients = 'pablo,ricardo,'
 
 
-def crea_cliente(cliente_nombre):
-    global clientes
+def create_client(client_name):
+    global clients
 
-    if cliente_nombre not in clientes:
-        clientes+=cliente_nombre
+    if client_name not in clients:
+        clients+= client_name
         add_comma()
     else:
         print('El cliente ya está en la lista de clientes.')
 
-def lista_clientes():
-    global clientes
-    print(clientes)
+def list_clients():
+    global clients
+    print(clients)
+
+
+def update_client(client_name, updated_client_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_client_name + ',')
+    else:
+        print("Client's not in clients list.")
 
 def add_comma():
-    global clientes
+    global clients
 
-    clientes+= ','
+    clients+= ','
 
 
-def escribir_bienvenida():
+def print_welcome():
     print("Bienvenido al software de ventas")
     print('*'*50)
-    print('Qué te gustaría hacer?')
-    print('[A]gregar cliente')
-    print('[E]liminar cliente')
+    print('What would like to do today?')
+    print('[C]reate client')
+    print('[U]pdate client')
+    print('[D]eleate client')
+
+
+def get_client_name():
+    return input("What's the client name?")
 
 
 if __name__ == '__main__':
-    escribir_bienvenida()
+    print_welcome()
+
     comando = input().upper()
-    if comando == 'A':
-        cliente_nombre=input('Nombre del cliente: ')
-        crea_cliente(cliente_nombre)
-        lista_clientes()
-    elif comando == 'E':
+
+    if comando == 'C':
+        client_name=get_client_name()
+        create_client(client_name)
+        list_clients()
+    elif comando == 'D':
+        client_name = get_client_name()
         pass
+    elif  comando == 'U':
+        client_name = get_client_name()
+        updated_client_name = input ("What's the updated client name?")
+        update_client(client_name,updated_client_name)
+        list_clients()
     else:
         print('Comando inválido.')

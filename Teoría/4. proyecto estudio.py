267625708f4a693729cde53132d9aa47e502b332
mauriciorey03 <edgarmeza909@gmail.com>
2023-08-17 13:39:21 -0500

    17/08

--- Teoría/4. proyecto estudio.py
+++ Teoría/4. proyecto estudio.py
@@ -10,6 +10,7 @@ def create_client(client_name):
     else:
         print('El cliente ya está en la lista de clientes.')
 
+
 def list_clients():
     global clients
     print(clients)
@@ -29,13 +30,26 @@ def add_comma():
     clients+= ','
 
 
+def delete_client():
+    global clients
+    
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', '')
+    else:
+        print("Client's not in clients list.")
+
+def search_client()
+
+
 def print_welcome():
     print("Bienvenido al software de ventas")
     print('*'*50)
     print('What would like to do today?')
     print('[C]reate client')
+    print('[R]ead client')
     print('[U]pdate client')
     print('[D]eleate client')
+    print('[S]earch client')
 
 
 def get_client_name():
@@ -51,13 +65,26 @@ if __name__ == '__main__':
         client_name=get_client_name()
         create_client(client_name)
         list_clients()
-    elif comando == 'D':
-        client_name = get_client_name()
-        pass
+    elif comando == 'R':
+        list_clients()
     elif  comando == 'U':
         client_name = get_client_name()
         updated_client_name = input ("What's the updated client name?")
         update_client(client_name,updated_client_name)
         list_clients()
+    elif comando == 'D':
+        client_name = get_client_name()
+        delete_client()
+        list_clients()
+        delete_client()
+    elif comando == 'S':
+        client_name = get_client_name()
+        found = search_client(client_name)
     else:
         print('Comando inválido.')
+
+
+        if found:
+            print("The client is in the client's list")
+        else:
+            print("The client: {} isn't in the client's list".format(client_name))
